3ba2ec0df5810e01ba483f620dde7d32ddf1aa44
rxjs-space <rxjs-space@outlook.com>
2018-04-09 15:59:13 +0800

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+const app = require('express')();
+const server = require('http').Server(app);
+// const WebSocket = require('ws');
+const WebSocketClient = require('./ws-client').WebSocketClient;
+const exec = require('child_process').exec;
+
+const port = process.env.PORT || 3006;
+const lymidUrl = 'ws://localhost:3005';
+// let ws = new WebSocket(lymidUrl);
+var wsc = new WebSocketClient();
+
+const doSomething = () => {
+  const dir = exec("ls -la", function(err, stdout, stderr) {
+    if (err) {
+      // should have err.code here?  
+    }
+    console.log(stdout);
+  });
+  
+  dir.on('exit', function (code) {
+    console.log(code);
+    // exit code is code
+  });
+};
+
+
+wsc.open(lymidUrl);
+wsc.onopen = function(e){
+	console.log("WebSocketClient connected.");
+	this.send("Hello World !");
+}
+wsc.onmessage = function(data,flags,number){
+  console.log(`WebSocketClient message #${number}: `,data);
+  switch(true) {
+    case data === 'do something':
+      doSomething();
+      break;
+  }
+}
+
+
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(port, function(){
+  console.log(`listening on *:${port}`);
+});
