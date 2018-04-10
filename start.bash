e748d55ff153c295b4ff61b68a1c818bccd71c7c
rxjs-space <rxjs-space@outlook.com>
2018-04-10 15:26:27 +0800

    adds npm i

--- start.bash
+++ start.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
+dirX=/home/timliu/codes/ly/lyback
 eval $(ssh-agent -s)
 wait
 ssh-add /home/timliu/.ssh/ly_envy_rsa
 wait
-git -C /home/timliu/codes/ly/lyback pull
+git -C $dirX pull
+wait
+npm --prefix $dirX install $dirX
